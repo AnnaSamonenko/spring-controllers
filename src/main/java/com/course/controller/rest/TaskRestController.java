0f7fc849a246ccb8defce021db0f630e1cc8f417
Anna Samonenko <Anna_Samonenko@epam.com>
2018-04-24 18:55:41 +0300

    Init project

---
+++ src/main/java/com/course/controller/rest/TaskRestController.java
@@ -0,0 +1,9 @@
+package com.course.controller.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskRestController {
+
+
+}
---
+++ src/test/java/com/course/TaskRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.course;
+
+import com.course.domain.Task;
+import com.course.repository.TaskRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ComponentScan
+public class TaskRepositoryTest {
+
+    @Autowired
+    private TaskRepository repository;
+
+    @Test
+    public void verifyAddTask() {
+        Task task = new Task("Spring", "Spring description");
+        repository.save(task);
+        Assert.assertTrue(repository.existsById(task.getId()));
+    }
+
+
+
+}
